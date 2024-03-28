5ef79772c56e78bcce42cbb0c8bca0676211b3f1
osamabakry <osamabakry@protonmail.com>
2024-03-28 23:39:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const typing = new AutoTyping('#animated-text', ['Designer', 'Developer','Freelancer','Photographer'], {
+    typeSpeed: 200,
+    deleteSpeed: 200,
+    waitBeforeDelete: 500,
+    waitBetweenWords: 0,
+});
+typing.start();
+
